24318c775ea04d52bd0ce43b36584bf9d43d2844
Nikita <nikitasa1997@gmail.com>
2021-04-25 22:38:00 +0300

    next

--- 1B.cpp
+++ 1B.cpp
@@ -35,19 +35,19 @@
 
 using schar  = signed char;
 using uchar  = unsigned char;
-using shrt   = short int;
-using ushrt  = unsigned short int;
-using uint   = unsigned int;
-using ulong  = unsigned long int;
-using llong  = long long int;
-using ullong = unsigned long long int;
+using shrt   = short;
+using ushrt  = unsigned short;
+using uint   = unsigned;
+using ulong  = unsigned long;
+using llong  = long long;
+using ullong = unsigned long long;
 using flt    = float;
 using dbl    = double;
 using ldbl   = long double;
 
 using namespace std;
 
-static string transform_numeration(const string &input) noexcept;
+static string transform_numeration(const string &) noexcept;
 
 int main() {
     ios_base::sync_with_stdio(false);
