6d4722c6fb7a18b771086cfeb836207bb6be7912
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-11-04 12:28:28 +0100

    leetcode: 1503

---
+++ leetcode/1503.cpp
@@ -0,0 +1,15 @@
+class Solution final {
+public:
+    constexpr int getLastMoment(
+        const int n,
+        const vector<int> &left,
+        const vector<int> &right
+    ) noexcept {
+        auto result(0);
+        if (!left.empty())
+            result = *max_element(left.cbegin(), left.cend());
+        if (!right.empty())
+            result = max(result, n - *min_element(right.cbegin(), right.cend()));
+        return result;
+    }
+};
