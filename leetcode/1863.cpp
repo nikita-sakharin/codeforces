a1061ace0019206d114e5e3e437a19d86e939411
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-11-04 17:40:17 +0100

    leetcode: 1863

--- leetcode/1863.cpp
+++ leetcode/1863.cpp
@@ -1,23 +1,7 @@
 class Solution final {
 public:
-    constexpr int subsetXORSum(const vector<int> &nums) const noexcept {
-        const size_t bound{1U << nums.size()};
-        auto result{0};
-        for (size_t i{0}; i < bound; ++i)
-            result += totalXOR(nums, i);
-        return result;
-    }
-
-    constexpr int totalXOR(
-        const vector<int> &nums,
-        const size_t mask
-    ) const noexcept {
-        const size_t size{nums.size()};
-        const bitset<numeric_limits<size_t>::digits> bits{mask};
-        auto result{0};
-        for (size_t i{0}; i < size; ++i)
-            if (bits[i])
-                result ^= nums[i];
-        return result;
+    inline int subsetXORSum(const vector<int> &nums) const noexcept {
+        const auto count{1 << (nums.size() - 1)};
+        return accumulate(nums.cbegin(), nums.cend(), 0, bit_or{}) * count;
     }
 };
