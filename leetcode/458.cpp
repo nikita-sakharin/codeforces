5be93545c8df9604a6eca0d5b28f98eb416ab4f8
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-11-04 16:27:36 +0100

    leetcode: 458

--- leetcode/458.cpp
+++ leetcode/458.cpp
@@ -1,11 +1,11 @@
 class Solution final {
 public:
-    constexpr int poorPigs(
+    inline int poorPigs(
         const int buckets,
         const int minutesToDie,
         const int minutesToTest
     ) const noexcept {
-        const auto count{minutesToTest / minutesToDie};
-        return max(1U, bit_width(static_cast<unsigned>(buckets) - 1));
+        const auto radix{minutesToTest / minutesToDie + 1};
+        return static_cast<int>(ceil(log2(buckets) / log2(radix)));
     }
 };
