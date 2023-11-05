c8766d70630360a004c63e888ab31a4a735ac0bf
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-11-05 21:47:58 +0000

    leetcode: 125

---
+++ leetcode/125.cpp
@@ -0,0 +1,21 @@
+class Solution final {
+private:
+    static inline const locale utf8{"C.UTF-8"};
+public:
+    constexpr bool isPalindrome(const string &s) const noexcept {
+        ptrdiff_t i{0}, j{static_cast<ptrdiff_t>(s.size()) - 1};
+        while (i < j) {
+            const auto left{s[i]}, right{s[j]};
+            if (!isalnum(left, utf8))
+                ++i;
+            else if (!isalnum(right, utf8))
+                --j;
+            else if (tolower(left, utf8) == tolower(right, utf8)) {
+                ++i;
+                --j;
+            } else
+                return false;
+        }
+        return true;
+    }
+};
