2cfd9e0759f416d36341d8c4781560be75c04ce0
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-11-06 14:04:33 +0000

    leetcode: 1

---
+++ leetcode/1.cpp
@@ -0,0 +1,19 @@
+class Solution final {
+public:
+    inline vector<int> twoSum(
+        const vector<int> &nums,
+        const int target
+    ) const noexcept {
+        const auto size{static_cast<int>(nums.size())};
+        unordered_map<int, int> numMap;
+        for (auto i{0}; i < size; ++i)
+            numMap[nums[i]] = i;
+        const auto end{numMap.cend()};
+        for (auto i{0}; i < size; ++i)
+            if (const auto iter{numMap.find(target - nums[i])};
+                iter != end && iter->second != i
+            )
+                return {i, iter->second};
+        return {};
+    }
+};
