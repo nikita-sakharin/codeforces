3c14b2c87a26b8f02e726ffddb2da98b783c5b27
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-11-23 21:34:25 +0000

    1493

--- leetcode/1493.cpp
+++ leetcode/1493.cpp
@@ -1,17 +1,15 @@
 class Solution final {
 public:
     constexpr int longestSubarray(const vector<int> &nums) const noexcept {
-        const auto numsEnd{nums.cend()};
-        size_t result{0}, lastCnt{0};
-        for (auto iter{nums.cbegin()}; iter != numsEnd; ) {
+        const auto numsBegin{nums.begin()}, numsEnd{nums.cend()};
+        auto iter{find(numsBegin, numsEnd, 0)};
+        auto lastCnt{iter - numsBegin}, result{lastCnt - (iter == numsEnd)};
+        while (iter != numsEnd) {
             const auto zeroCnt{find(iter, numsEnd, 1) - iter};
             iter += zeroCnt;
             const auto oneCnt{find(iter, numsEnd, 0) - iter};
             iter += oneCnt;
-            if (zeroCnt == 0)
-                result = oneCnt - 1;
-            else if (zeroCnt == 1)
-                result = max(result, lastCnt + oneCnt);
+            result = max(result, zeroCnt == 1 ? lastCnt + oneCnt : oneCnt);
             lastCnt = oneCnt;
         }
         return static_cast<int>(result);
