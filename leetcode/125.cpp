b7cf76397c6df0d79815e4a8a292de39999d4fa0
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-11-26 13:49:20 +0000

    125

--- leetcode/125.cpp
+++ leetcode/125.cpp
@@ -4,9 +4,9 @@ private:
 
 public:
     constexpr bool isPalindrome(const string &s) const noexcept {
-        ptrdiff_t i{0}, j{static_cast<ptrdiff_t>(s.size()) - 1};
+        ptrdiff_t i{0}, j{ptrdiff_t(s.size()) - 1};
         while (i < j) {
-            const auto left{s[i]}, right{s[j]};
+            const auto left{s[size_t(i)]}, right{s[size_t(j)]};
             if (!isalnum(left, classic))
                 ++i;
             else if (!isalnum(right, classic))
