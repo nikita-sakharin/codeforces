ebc9073deda14364b343418f64e315723a830e22
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-11-26 14:28:14 +0000

    977

--- leetcode/977.cpp
+++ leetcode/977.cpp
@@ -1,17 +1,20 @@
 class Solution final {
 public:
-    inline vector<int> sortedSquares(const vector<int> &nums) noexcept {
-        const auto size{ssize(nums)};
-        vector<int> result(size);
-        ptrdiff_t i{0}, j{size - 1}, index{size};
-        while (i <= j)
-            if (nums[i] * nums[i] < nums[j] * nums[j]) {
-                result[--index] = nums[j] * nums[j];
+    inline vector<int> sortedSquares(const vector<int> &nums) const noexcept {
+        size_t index{nums.size()};
+        vector<int> result(index);
+        ptrdiff_t i{0}, j{ptrdiff_t(index) - 1};
+        while (i <= j) {
+            const auto productI{nums[size_t(i)] * nums[size_t(i)]},
+                productJ{nums[size_t(j)] * nums[size_t(j)]};
+            if (productI < productJ) {
+                result[--index] = productJ;
                 --j;
             } else {
-                result[--index] = nums[i] * nums[i];
+                result[--index] = productI;
                 ++i;
             }
+        }
         return result;
     }
 };
