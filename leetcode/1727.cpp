1326b102e20f420ea9620cdc4ddb5093b55eae83
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-11-26 16:56:54 +0000

    1727

---
+++ leetcode/1727.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    constexpr int largestSubmatrix(
+        vector<vector<int>> &matrix
+    ) const noexcept {
+        const auto m{matrix.size()}, n{matrix.front().size()};
+        for (size_t i{1}; i < m; ++i)
+            for (size_t j{0}; j < n; ++j)
+                if (matrix[i][j] != 0)
+                    matrix[i][j] = matrix[i - 1][j] + 1;
+        int result{0};
+        for (size_t i{0}; i < m; ++i) {
+            sort(matrix[i].begin(), matrix[i].end());
+            for (size_t j{0}; j < n; ++j)
+                result = max(result, matrix[i][n - j - 1] * int(j + 1));
+        }
+        return result;
+    }
+};
