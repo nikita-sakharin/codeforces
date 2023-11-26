4ffad0ba62f5ce87b3e37e010d8abac05b432ae0
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-11-26 18:19:08 +0000

    1493

--- leetcode/1493.cpp
+++ leetcode/1493.cpp
@@ -1,14 +1,23 @@
 class Solution final {
+private:
+    template<class InputIt, class T>
+    static constexpr pair<InputIt, ptrdiff_t> adjacent_count(
+        const InputIt first, const InputIt last,
+        const T &value
+    ) noexcept {
+        const auto iter{find(first, last, value)};
+        return {iter, iter - first};
+    }
+
 public:
     constexpr int longestSubarray(const vector<int> &nums) const noexcept {
-        const auto numsBegin{nums.begin()}, numsEnd{nums.cend()};
-        auto iter{find(numsBegin, numsEnd, 0)};
-        auto lastCnt{iter - numsBegin}, result{lastCnt - (iter == numsEnd)};
+        const auto numsEnd{nums.cend()};
+        auto [iter, lastCnt] = adjacent_count(nums.cbegin(), numsEnd, 0);
+        auto result{lastCnt - (iter == numsEnd)};
         while (iter != numsEnd) {
-            const auto zeroCnt{find(iter, numsEnd, 1) - iter};
-            iter += zeroCnt;
-            const auto oneCnt{find(iter, numsEnd, 0) - iter};
-            iter += oneCnt;
+            ptrdiff_t zeroCnt, oneCnt;
+            tie(iter, zeroCnt) = adjacent_count(iter, numsEnd, 1);
+            tie(iter, oneCnt) = adjacent_count(iter, numsEnd, 0);
             result = max(result, zeroCnt == 1 ? lastCnt + oneCnt : oneCnt);
             lastCnt = oneCnt;
         }
