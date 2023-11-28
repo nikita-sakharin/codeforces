79d16046a47b2cd74da330c99ada00fb6eb641b8
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-11-28 20:20:34 +0000

    13

---
+++ leetcode/13.cpp
@@ -0,0 +1,35 @@
+class Solution final {
+    static constexpr int letterToInt(const char ch) noexcept {
+        switch (ch) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                unreachable();
+        }
+    }
+
+public:
+    constexpr int romanToInt(const string &str) const noexcept {
+        auto result{0}, prevInt{0};
+        for (const auto &chr : str) {
+            const auto toInt{letterToInt(chr)};
+            if (toInt < prevInt)
+                result -= toInt;
+            else
+                result += toInt;
+        }
+        return result;
+    }
+};
