c16a07ab799091be5a421417264307d0622c1073
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-12-17 18:13:02 +0000

    leetcode/21.cpp

--- leetcode/21.cpp
+++ leetcode/21.cpp
@@ -8,7 +8,7 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution final {
 public:
     constexpr ListNode *mergeTwoLists(
         ListNode *list1,
