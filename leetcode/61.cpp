6234863a2d8da4b27f38e067379ab3efa298b6df
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-01-03 17:45:40 +0000

    leetcode/61.cpp

---
+++ leetcode/61.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution final {
+private:
+    static constexpr pair<ListNode *, size_t> listLastElement(
+        ListNode * head
+    ) noexcept {
+        if (!head)
+            return {nullptr, 0};
+        size_t result{1};
+        while (head->next) {
+            head = head->next;
+            ++result;
+        }
+        return {head, result};
+    }
+
+    static constexpr ListNode *listNthElement(
+        ListNode * head,
+        size_t index
+    ) noexcept {
+        for (; index != 0; --index)
+            head = head->next;
+        return head;
+    }
+
+public:
+    constexpr ListNode *rotateRight(
+        ListNode *head,
+        const int k
+    ) const noexcept {
+        const auto [last, size] = listLastElement(head);
+        if (!last)
+            return nullptr;
+        last->next = head;
+        const auto middle = listNthElement(head, size - k % size - 1);
+        head = middle->next;
+        middle->next = nullptr;
+        return head;
+    }
+};
