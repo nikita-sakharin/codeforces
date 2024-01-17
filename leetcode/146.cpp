6f600114b6be8794859dc33d88b743462547aa4a
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-01-17 22:54:46 +0000

    leetcode/146

---
+++ leetcode/146.cpp
@@ -0,0 +1,42 @@
+class LRUCache final {
+private:
+    unordered_map<int, list<pair<int, int>>::iterator> index{};
+    list<pair<int, int>> elements{};
+    size_t maxSize{0};
+
+public:
+    inline LRUCache(int capacity) noexcept : maxSize(capacity) {}
+
+    inline int get(const int key) noexcept {
+        const auto iter{index.find(key)};
+        if (iter != index.end()) {
+            const auto value{iter->second->second};
+            elements.erase(iter->second);
+            index.erase(iter);
+            elements.emplace_back(key, value);
+            index.emplace(key, --elements.end());
+            return value;
+        }
+        return -1;
+    }
+
+    inline void put(const int key, const int value) noexcept {
+        const auto iter{index.find(key)};
+        if (iter != index.end()) {
+            elements.erase(iter->second);
+            index.erase(iter);
+        }
+        if (index.size() >= maxSize) {
+            index.erase(elements.front().first);
+            elements.pop_front();
+        }
+        elements.emplace_back(key, value);
+        index.emplace(key, --elements.end());
+    }
+};
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
