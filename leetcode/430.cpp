e444772b857c4f496d83ccaadd29a27b784d5a3c
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-01-17 20:28:27 +0000

    leetcode/430

---
+++ leetcode/430.cpp
@@ -0,0 +1,35 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+class Solution final {
+public:
+    inline Node *flatten(Node * const head) const noexcept {
+        if (!head)
+            return nullptr;
+        stack<Node *> s;
+        auto current{head};
+        while (current->next || current->child || !s.empty()) {
+            if (current->child) {
+                if (current->next)
+                    s.push(current->next);
+                current->next = current->child;
+                current->next->prev = current;
+                current->child = nullptr;
+            }
+            if (!current->next) {
+                current->next = s.top();
+                current->next->prev = current;
+                s.pop();
+            }
+            current = current->next;
+        }
+        return head;
+    }
+};
