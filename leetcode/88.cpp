428fbfef074bd75d03e1ef5673aea0958a3c6ee2
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-01-18 22:38:22 +0000

    leetcode/88.cpp

--- leetcode/88.cpp
+++ leetcode/88.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> &nums2, int n
     ) const noexcept {
         --m, --n;
-        for (ptrdiff_t i{m + n - 1}; i >= 0; --i)
+        for (ptrdiff_t i{m + n + 1}; i >= 0; --i)
             if (m < 0 || (n >= 0 && nums1[m] < nums2[n]))
                 nums1[size_t(i)] = nums2[size_t(n--)];
             else
