352535f53c2a7b9edfb7a744988494644a48972b
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-01-18 22:06:02 +0000

    leetcode/66.cpp

--- leetcode/66.cpp
+++ leetcode/66.cpp
@@ -1,15 +1,14 @@
 class Solution final {
 public:
     inline vector<int> plusOne(vector<int> &digits) const noexcept {
-        auto carry{false};
-        for (ptrdiff_t i(digits.size() - 1); i >= 0; --i) {
-            if (++digits[i] < 10) {
+        auto carry{true};
+        for (ptrdiff_t i(digits.size() - 1); i >= 0 && carry; --i)
+            if (++digits[i] < 10)
                 carry = false;
-                break;
+            else {
+                carry = true;
+                digits[i] = 0;
             }
-            carry = true;
-            digits[i] = 0;
-        }
         if (carry)
             digits.insert(digits.cbegin(), 1);
         return digits;
