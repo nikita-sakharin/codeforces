2506806e78eaee5d654b6824f594a50ffdd57c56
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-01-29 05:51:32 +0000

    leetcode/94

---
+++ leetcode/94.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution final {
+private:
+    template<class T>
+    using Stack = stack<T, vector<T>>;
+
+public:
+    inline vector<int> inorderTraversal(
+        const TreeNode *treeNode
+    ) const noexcept {
+        vector<int> result{};
+        Stack<const TreeNode *> s{};
+        while (treeNode || !s.empty()) {
+            while (treeNode) {
+                s.push(treeNode);
+                treeNode = treeNode->left;
+            }
+            treeNode = s.top();
+            s.pop();
+            result.push_back(treeNode->val);
+            treeNode = treeNode->right;
+        }
+
+        return result;
+    }
+};
