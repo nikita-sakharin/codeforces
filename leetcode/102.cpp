4488a91a5cd28cb53078dc1358010e21c0b9f47e
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-01-31 17:52:07 +0000

    leetcode/102

---
+++ leetcode/102.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    inline vector<vector<int>> levelOrder(
+        const TreeNode * const root
+    ) const noexcept {
+        vector<vector<int>> result{};
+        queue<pair<const TreeNode *, size_t>> q{};
+        q.emplace(root, 0);
+        while (!q.empty()) {
+            const auto [treeNode, level]{q.front()};
+            q.pop();
+            if (result.size() <= level)
+                result.emplace_back();
+            result.back().push_back(treeNode->val);
+            if (treeNode->left)
+                q.emplace(treeNode->left, level + 1);
+            if (treeNode->right)
+                q.emplace(treeNode->right, level + 1);
+        }
+        return result;
+    }
+};
