2f4838130be7f6281feba35028eade656fdf895c
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-02-02 21:38:30 +0000

    leetcode/231

--- leetcode/231.cpp
+++ leetcode/231.cpp
@@ -1,6 +1,6 @@
 class Solution final {
 public:
-    constexpr bool isPowerOfTwo(int n) const noexcept {
+    constexpr bool isPowerOfTwo(const int n) const noexcept {
         return n > 0 && (n & (n - 1)) == 0;
     }
 };
