fc0a8278b2d9f93dee51dc81084883b2b6bda814
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-02-11 17:33:27 +0000

    leetcode/26

---
+++ leetcode/26.cpp
@@ -0,0 +1,11 @@
+class Solution final {
+public:
+    constexpr int removeDuplicates(vector<int> &nums) const noexcept {
+        const auto size{nums.size()};
+        size_t newSize{1};
+        for (size_t i{1}; i < size; ++i)
+            if (nums[i - 1] != nums[i])
+                nums[newSize++] = nums[i];
+        return newSize;
+    }
+};
