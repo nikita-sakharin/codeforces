0b536e2d633841ba0eb35044d751640371a0db65
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-02-11 20:15:26 +0000

    leetcode/108

---
+++ leetcode/108.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution final {
+private:
+    template<class T>
+    using Stack = stack<T, vector<T>>;
+
+public:
+    inline TreeNode *sortedArrayToBST(
+        const vector<int> &nums
+    ) const noexcept {
+        TreeNode *root{};
+        Stack<tuple<TreeNode **, size_t, size_t>> s{};
+        s.emplace(&root, 0, nums.size());
+        do {
+            const auto [ptr, first, last]{s.top()};
+            s.pop();
+            const auto middle{first + ((last - first) >> 1)};
+            const auto treeNode{new TreeNode(nums[middle])};
+            *ptr = treeNode;
+            if (first < middle)
+                s.emplace(&treeNode->left, first, middle);
+            if (middle + 1 < last)
+                s.emplace(&treeNode->right, middle + 1, last);
+        } while (!s.empty());
+        return root;
+    }
+};
