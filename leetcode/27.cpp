82879f7f183d45293531a77f5a8bcbfee29add7e
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-02-11 17:40:02 +0000

    leetcode/27

---
+++ leetcode/27.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    constexpr int removeElement(
+        vector<int> &nums,
+        const int val
+    ) const noexcept {
+        const auto size{nums.size()};
+        size_t newSize{0};
+        for (size_t i{0}; i < size; ++i)
+            if (nums[i] != val)
+                nums[newSize++] = nums[i];
+        nums.resize(newSize);
+        return int(newSize);
+    }
+};
