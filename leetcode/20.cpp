7a8723efbc78c964b212d66f33cf9d867165164e
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-02-16 18:19:28 +0000

    leetcode/20

--- leetcode/20.cpp
+++ leetcode/20.cpp
@@ -3,13 +3,17 @@ private:
     template<class T>
     using Stack = stack<T, vector<T>>;
 
+    static constexpr char getClosing(const char opening) noexcept {
+        return (opening + 1) | 1;
+    }
+
 public:
     inline bool isValid(const string &str) const noexcept {
         Stack<char> s{};
         for (const auto &ch : str)
             if (ch == '(' || ch == '[' || ch == '{')
                 s.push(ch);
-            else if (s.empty() || ((s.top() + 1) | 1) != ch)
+            else if (s.empty() || getClosing(s.top()) != ch)
                 return false;
             else
                 s.pop();
