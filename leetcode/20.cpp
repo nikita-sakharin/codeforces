48d4ab93f8df5a690b5b919233021de3e10fa961
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-02-16 00:39:56 +0000

    leetcode/20

---
+++ leetcode/20.cpp
@@ -0,0 +1,24 @@
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution final {
+private:
+    template<class T>
+    using Stack = stack<T, vector<T>>;
+
+public:
+    inline bool isValid(const string &str) const noexcept {
+        Stack<char> s{};
+        for (const auto &ch : str)
+            if (ch == '(' || ch == '[' || ch == '{')
+                s.push(ch);
+            else if (!s.empty() && ((s.top() + 1) | 1) == ch)
+                s.pop();
+            else
+                return false;
+        return s.empty();
+    }
+};
