f7d3980599f4c371bfa19b28960a6113b0897a42
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-05-21 22:24:02 +0100

    leetcode: 912

--- leetcode/912.cpp
+++ leetcode/912.cpp
@@ -3,40 +3,36 @@ private:
     template<class Iter>
     static void merge(Iter first, const Iter middle, const Iter last) noexcept {
         queue<typename iterator_traits<Iter>::value_type> buffer{};
-        Iter second{middle};
-        while ((first != middle || !buffer.empty()) && second != last) {
-            if (first != middle)
+        auto second{middle};
+        while (first != last) {
+            if (first < middle)
                 buffer.push(move(*first));
-            if (*second < buffer.front())
-                *first++ = move(*second++);
-            else {
-                *first++ = move(buffer.front());
+            if (!buffer.empty() && second < last && *second < buffer.front()) {
+                *first = move(*second++);
+            } else if (!buffer.empty()) {
+                *first = move(buffer.front());
                 buffer.pop();
             }
-        }
-        while (!buffer.empty()) {
-            *first++ = move(buffer.front());
-            buffer.pop();
+            ++first;
         }
     }
 
     template<class Iter>
     static void mergeSort(const Iter first, const Iter last) noexcept {
         const auto size{distance(first, last)}, half{size >> 1};
-        ptrdiff_t width{1};
+        typename iterator_traits<Iter>::difference_type width{1};
         while (width < size) {
-            const auto nextWidth{width <= half ? width << 1 : size};
             auto n{size};
             auto iter{first};
             while (iter != last) {
-                const auto left{min(width, n)}, right{min(width, n - left)}; // TODO
+                const auto left{min(width, n)}, right{min(width, n - left)};
                 const auto middle{next(iter, left)},
                     nextIter{next(middle, right)};
                 merge(iter, middle, nextIter);
                 n -= left + right;
                 iter = nextIter;
             }
-            width = nextWidth;
+            width = width <= half ? width << 1 : size;
         }
     }
 
