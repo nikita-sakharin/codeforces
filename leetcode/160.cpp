0c26a487e29a77d6ff0509403eb79905f87e0ea9
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-06-01 00:53:55 +0100

    leetcode: 160

---
+++ leetcode/160.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    static constexpr size_t listSize(const ListNode *head) noexcept {
+        size_t size{0};
+        while (head) {
+            head = head->next;
+            ++size;
+        }
+        return size;
+    }
+
+    static constexpr ListNode *nextNode(ListNode *listNode, size_t n) noexcept {
+        for (; n != 0; --n)
+            listNode = listNode->next;
+        return listNode;
+    }
+
+public:
+    constexpr ListNode *getIntersectionNode(
+        ListNode *headA,
+        ListNode *headB
+    ) const noexcept {
+        const auto sizeA{listSize(headA)}, sizeB{listSize(headB)};
+        if (sizeA < sizeB)
+            headB = nextNode(headB, sizeB - sizeA);
+        else if (sizeB < sizeA)
+            headA = nextNode(headA, sizeA - sizeB);
+        while (headA != headB) {
+            headA = headA->next;
+            headB = headB->next;
+        }
+        return headA;
+    }
+};
