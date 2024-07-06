49254a782b8a8d4fc24af07d1b8ceaead3c6ec36
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-07-06 09:52:42 +0100

    leetcode: 409

---
+++ leetcode/409.cpp
@@ -0,0 +1,23 @@
+class Solution final {
+private:
+    static constexpr size_t letters_count{26};
+
+    static constexpr size_t ord(const char ch) noexcept {
+        return ch >= 'a' ? ch - 'a' + 26 : ch - 'A';
+    }
+
+public:
+    constexpr int longestPalindrome(const string &s) noexcept {
+        array<size_t, letters_count << 1> count{};
+        for (const auto ch : s)
+            ++count[ord(ch)];
+
+        size_t result{0};
+        bool odd{false};
+        for (const auto c : count) {
+            result += c & ~1UZ;
+            odd = odd | (c & 1);
+        }
+        return int(result) + odd;
+    }
+};
