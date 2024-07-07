baf5e46a99cfcae01014f6702c6d5d4ca0b8ab2c
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-07-07 22:56:59 +0100

    leetcode: 45

--- leetcode/45.cpp
+++ leetcode/45.cpp
@@ -2,12 +2,12 @@ class Solution final {
 public:
     constexpr int jump(const vector<int> &nums) noexcept {
         const auto last{nums.size() - 1};
-        auto i{0UZ}, maxJump{0UZ}, result{0UZ};
+        auto i{0UZ}, nextI{0UZ}, result{0UZ};
 
-        while (maxJump < last) {
-            const auto stop{maxJump};
-            for (maxJump = 0; i <= stop; ++i)
-                maxJump = max(maxJump, i + min(last - i, size_t(nums[i])));
+        while (nextI < last) {
+            const auto stop{nextI};
+            for (nextI = 0; i <= stop; ++i)
+                nextI = max(nextI, i + min(last - i, size_t(nums[i])));
             ++result;
         }
         return int(result);
