4aa90cd752eb9f09573aa154e3048d84698c8406
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-07-10 23:15:35 +0100

    leetcode: 455

---
+++ leetcode/455.cpp
@@ -0,0 +1,16 @@
+class Solution final {
+public:
+    constexpr int findContentChildren(vector<int> &g, vector<int> &s) noexcept {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        const auto gEnd{g.cend()}, sEnd{s.cend()};
+        auto gIter{g.cbegin()}, sIter{s.cbegin()};
+        while (gIter != gEnd && sIter != sEnd) {
+            if (*gIter <= *sIter)
+                ++gIter;
+            ++sIter;
+        }
+        return gIter - g.cbegin();
+    }
+};
