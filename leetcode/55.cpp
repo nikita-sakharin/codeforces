c519c80489653c93df71f813fbf91bca8a1bdd7c
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-07-10 20:10:44 +0100

    leetcode: 55

---
+++ leetcode/55.cpp
@@ -0,0 +1,12 @@
+class Solution final {
+public:
+    constexpr bool canJump(const vector<int> &nums) noexcept {
+        const auto size{nums.size()};
+        for (auto i{0UZ}, maxI{0UZ}; i < size; ++i) {
+            if (i > maxI)
+                return false;
+            maxI = max(maxI, i + min(size - i - 1, size_t(nums[i])));
+        }
+        return true;
+    }
+};
