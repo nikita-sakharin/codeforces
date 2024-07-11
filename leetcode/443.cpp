01bdfd5e609e72f25da5d0dae9a84be2fa0033a1
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-07-11 22:55:25 +0100

    leetcode: 443

--- leetcode/443.cpp
+++ leetcode/443.cpp
@@ -4,8 +4,8 @@ public:
         const auto size{chars.size()};
         const auto ptrEnd{chars.data() + size};
         auto ptr{chars.data() + 1};
-        size_t cnt{1};
-        for (size_t i{1}; i < size; ++i) {
+        auto cnt{1UZ};
+        for (auto i{1UZ}; i < size; ++i) {
             if (chars[i - 1] != chars[i]) {
                 if (cnt > 1)
                     ptr = to_chars(ptr, ptrEnd, cnt).ptr;
