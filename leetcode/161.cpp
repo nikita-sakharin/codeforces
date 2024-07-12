53d0d6e6e25e43984a873fd37ca95cbf58c81cb8
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-07-12 23:59:53 +0100

    leetcode: 161

--- leetcode/161.cpp
+++ leetcode/161.cpp
@@ -10,7 +10,7 @@ public:
         if (sSize - tSize > 1)
             return false;
 
-        for (size_t i{0}; i < tSize; ++i)
+        for (auto i{0UZ}; i < tSize; ++i)
             if (s[i] != t[i]) {
                 return equal(
                     s.cbegin() + i + 1, s.cend(),
