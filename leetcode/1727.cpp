4bc1a2785ba9e03a611a48ecca125dacd0c9b731
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-07-13 00:08:30 +0100

    leetcode: 1727

--- leetcode/1727.cpp
+++ leetcode/1727.cpp
@@ -4,14 +4,14 @@ public:
         vector<vector<int>> &matrix
     ) const noexcept {
         const auto m{matrix.size()}, n{matrix.front().size()};
-        for (size_t i{1}; i < m; ++i)
-            for (size_t j{0}; j < n; ++j)
+        for (auto i{1UZ}; i < m; ++i)
+            for (auto j{0UZ}; j < n; ++j)
                 if (matrix[i][j] != 0)
                     matrix[i][j] = matrix[i - 1][j] + 1;
-        int result{0};
-        for (size_t i{0}; i < m; ++i) {
+        auto result{0};
+        for (auto i{0UZ}; i < m; ++i) {
             sort(matrix[i].begin(), matrix[i].end());
-            for (size_t j{0}; j < n; ++j)
+            for (auto j{0UZ}; j < n; ++j)
                 result = max(result, matrix[i][n - j - 1] * int(j + 1));
         }
         return result;
