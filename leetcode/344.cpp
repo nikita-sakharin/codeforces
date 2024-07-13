67d6aa248a21c1e3f767148a74b828eaf0b0a2d5
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-07-13 23:28:17 +0100

    leetcode: 344

--- leetcode/344.cpp
+++ leetcode/344.cpp
@@ -1,7 +1,7 @@
 class Solution final {
 public:
     constexpr void reverseString(vector<char> &s) const noexcept {
-        for (size_t i{0}, j{s.size() - 1}; i < j; ++i, --j)
+        for (auto i{0UZ}, j{s.size() - 1}; i < j; ++i, --j)
             swap(s[i], s[j]);
     }
 };
