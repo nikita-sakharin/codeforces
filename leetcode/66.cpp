6d69175da86f0fcb99a858a3d0a597dabaa07cfa
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-07-14 13:07:28 +0100

    leetcode: 66

--- leetcode/66.cpp
+++ leetcode/66.cpp
@@ -1,6 +1,6 @@
 class Solution final {
 public:
-    inline vector<int> plusOne(vector<int> &digits) const noexcept {
+    constexpr vector<int> plusOne(vector<int> &digits) const noexcept {
         auto carry{true};
         for (ptrdiff_t i(digits.size() - 1); i >= 0 && carry; --i)
             if (++digits[i] < 10)
