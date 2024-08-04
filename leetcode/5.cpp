78259e825ac90d065e9e6d47588043f460cac736
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-08-04 15:22:10 +0100

    leetcode: 5

--- leetcode/5.cpp
+++ leetcode/5.cpp
@@ -1,33 +1,44 @@
 class Solution final {
+private:
+    template<class Iter>
+    using Difference = iterator_traits<Iter>::difference_type;
+
+    template<class Iter, class Func, class Container = deque<Difference<Iter>>>
+    static constexpr void findPalindromes(
+        const Iter first,
+        const Iter last,
+        const bool isOdd,
+        Func func
+    ) noexcept {
+        const auto size{distance(first, last)};
+        Container buffer{};
+        for (Difference<Iter> i{0}, left{0}, right{-1}; i < size; ++i) {
+            auto k{i > right ? isOdd : min(buffer[left + (right + !isOdd - i)], right + 1 - i)};
+            while (k < i + isOdd && k < size - i && first[i - !isOdd - k] == first[i + k])
+                ++k;
+            buffer.push_back(k);
+            if (i - 1 + k > right) {
+                left = i + isOdd - k;
+                right = i - 1 + k;
+            }
+            func(first + i + isOdd - k, first + i + k);
+        }
+    }
+
 public:
-    inline string longestPalindrome(const string &s) const noexcept {
-        const auto size{s.size()};
+    constexpr string longestPalindrome(const string &s) const noexcept {
         string_view result{};
-        vector<size_t> even(size), odd(size);
-        for (ptrdiff_t i{0}, left{0}, right{-1}; i < size; ++i) {
-            auto k{i > right ? 0 : min(ptrdiff_t(even[left + (right + 1 - i)]), right + 1 - i)};
-            while (k < i && i + k < size && s[size_t(i - 1 - k)] == s[size_t(i + k)])
-                ++k;
-            even[i] = k;
-            if (i - 1 + k > right) {
-                left = i - k;
-                right = i - 1 + k;
+        const auto func{
+            [&result](
+                const auto first,
+                const auto last
+            ) constexpr noexcept -> void {
+                if (size_t(distance(first, last)) > result.size())
+                    result = string_view(first, last);
             }
-            if (const size_t newSize(k << 1); newSize > result.size())
-                result = string_view(s.data() + i - k, newSize);
-        }
-        for (ptrdiff_t i{0}, left{0}, right{-1}; i < size; ++i) {
-            auto k{i > right ? 1 : min(ptrdiff_t(odd[left + (right - i)]), right + 1 - i)};
-            while (k <= i && i + k < size && s[size_t(i - k)] == s[size_t(i + k)])
-                ++k;
-            odd[i] = k;
-            if (i - 1 + k > right) {
-                left = i + 1 - k;
-                right = i - 1 + k;
-            }
-            if (const size_t newSize((k << 1) - 1); newSize > result.size())
-                result = string_view(s.data() + i + 1 - k, newSize);
-        }
+        };
+        findPalindromes(s.cbegin(), s.cend(), true, func);
+        findPalindromes(s.cbegin(), s.cend(), false, func);
         return string(result);
     }
 };
