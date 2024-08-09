1fba1b625dc072f727f33a8cd1efee55c956c236
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-08-09 11:35:27 +0100

    leetcode: 2221

--- leetcode/2221.cpp
+++ leetcode/2221.cpp
@@ -24,33 +24,35 @@ private:
         ForwardIter first,
         const size_t rowIndex
     ) noexcept {
-        auto binomial = *first++ = 1;
+        using Value = iterator_traits<ForwardIter>::value_type;
+
+        auto binomial = *first++ = Value{1};
         for (auto k{0UZ}, exp2{0UZ}, exp5{0UZ}; k < rowIndex; ++first, ++k) {
             auto multiplier{rowIndex - k}, divider{k + 1};
             auto diff{0U};
 
-            const auto multiplierTwos{countr_zero(multiplier)};
+            const auto multiplierTwos{uint(countr_zero(multiplier))};
             exp2 += multiplierTwos;
             multiplier >>= multiplierTwos;
             tie(multiplier, diff) = factorize(multiplier, size_t{5});
             exp5 += diff;
-            binomial = binomial * multiplier % mod;
+            binomial = binomial * Value(multiplier) % Value{mod};
 
-            const auto dividerTwos{countr_zero(divider)};
+            const auto dividerTwos{uint(countr_zero(divider))};
             exp2 -= dividerTwos;
             divider >>= dividerTwos;
             tie(divider, diff) = factorize(divider, size_t{5});
             exp5 -= diff;
-            binomial = binomial * mod10Inverse[divider % mod] % mod;
+            binomial = binomial * mod10Inverse[divider % mod] % Value{mod};
 
             if (!exp2 && !exp5)
                 *first = binomial;
             else if (exp2 && !exp5)
-                *first = binomial * remainders[exp2 & 0X3] % mod;
+                *first = binomial * remainders[exp2 & 0X3] % Value{mod};
             else if (!exp2 && exp5)
-                *first = binomial * 5 % mod;
+                *first = binomial * Value{5} % Value{mod};
             else
-                *first = 0;
+                *first = Value{0};
         }
     }
 
@@ -61,7 +63,7 @@ public:
         pascalTriangleRow(row.begin(), size - 1);
         auto result{0};
         for (auto i{0UZ}; i < size; ++i)
-            result = (result + nums[i] * row[i]) % mod;
+            result = (result + nums[i] * row[i]) % int{mod};
         return result;
     }
 };
