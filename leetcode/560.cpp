48072fb2efc246ace336a9e2fec3e74e79412ea0
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-08-14 18:12:21 +0100

    leetcode: 560

--- leetcode/560.cpp
+++ leetcode/560.cpp
@@ -1,11 +1,11 @@
 class Solution final {
 private:
-    static constexpr int countSubarray(
-        const unordered_map<int, int> &numMap,
+    static constexpr size_t countSubarray(
+        const unordered_map<int, size_t> &numMap,
         const int num,
         const int k
     ) noexcept {
-        auto result{0};
+        auto result{0UZ};
         if (num == k)
             ++result;
         if (const auto iter{numMap.find(num + k)}; iter != numMap.cend())
@@ -14,12 +14,12 @@ private:
     }
 
     static inline void deleteNum(
-        unordered_map<int, int> &numMap,
+        unordered_map<int, size_t> &numMap,
         const int num
     ) noexcept {
         const auto iter{numMap.find(num)};
         --iter->second;
-        if (iter->second <= 0)
+        if (iter->second == 0)
             numMap.erase(iter);
     }
 
@@ -29,17 +29,17 @@ public:
         const int k
     ) const noexcept {
         const auto size{nums.size()};
-        unordered_map<int, int> numMap{{nums[0], 1}};
+        unordered_map<int, size_t> numMap{{nums[0], 1}};
         for (auto i{1UZ}; i < size; ++i) {
             nums[i] += nums[i - 1];
             ++numMap[nums[i]];
         }
 
-        auto result{0};
+        auto result{0UZ};
         for (const auto num : nums) {
             deleteNum(numMap, num);
             result += countSubarray(numMap, num, k);
         }
-        return result;
+        return int(result);
     }
 };
