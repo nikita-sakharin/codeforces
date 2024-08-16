c29486bfa04fb5851ac342ae69f4a001ca039264
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-08-16 12:31:14 +0100

    leetcode: 1

--- leetcode/1.cpp
+++ leetcode/1.cpp
@@ -6,14 +6,12 @@ public:
     ) const noexcept {
         const auto size{nums.size()};
         unordered_map<int, size_t> numMap{};
-        for (auto i{0UZ}; i < size; ++i)
-            numMap[nums[i]] = i;
         const auto end{numMap.cend()};
-        for (auto i{0UZ}; i < size; ++i)
-            if (const auto iter{numMap.find(target - nums[i])};
-                iter != end && iter->second != i
-            )
-                return {int(i), int(iter->second)};
+        for (auto i{0UZ}; i < size; ++i) {
+            if (const auto iter{numMap.find(target - nums[i])}; iter != end)
+                return {int(iter->second), int(i)};
+            numMap[nums[i]] = i;
+        }
         return {};
     }
 };
