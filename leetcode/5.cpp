82737c416146eed2d670728b88e985d2162f86af
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-08-18 18:56:24 +0100

    leetcode: 5

--- leetcode/5.cpp
+++ leetcode/5.cpp
@@ -4,7 +4,6 @@ private:
     static constexpr void findPalindromes(
         const Iter first,
         const Iter last,
-        const bool isOdd,
         Func func
     ) noexcept {
         using Difference = iterator_traits<Iter>::difference_type;
@@ -12,19 +11,24 @@ private:
         if (first == last)
             return;
         const auto size{distance(first, last)};
-        vector<Difference> buffer(size - !isOdd);
-        for (Difference i{!isOdd}, left{0}, right{0}; i < size; ++i) {
-            const auto bound{min(i + isOdd, size - i)};
-            auto k{i >= right ? isOdd : min(buffer[left + (right - 1 - i)], right - i)};
-            while (k < bound && first[i - !isOdd - k] == first[i + k])
-                ++k;
-            buffer[i - !isOdd] = k;
-            if (i + k > right) {
-                left = i + isOdd - k;
-                right = i + k;
+        vector<Difference> buffer(size);
+        for (const bool isOdd : {false, true}) {
+            for (Difference i{!isOdd}, left{0}, right{0}; i < size; ++i) {
+                const auto bound{min(i + isOdd, size - i)};
+                auto k{i >= right
+                    ? isOdd
+                    : min(buffer[left + (right - 1 - i)], right - i)
+                };
+                while (k < bound && first[i - !isOdd - k] == first[i + k])
+                    ++k;
+                buffer[i - !isOdd] = k;
+                if (i + k > right) {
+                    left = i + isOdd - k;
+                    right = i + k;
+                }
+                if (k != 0)
+                    func(first + i + isOdd - k, first + i + k);
             }
-            if (k != 0)
-                func(first + i + isOdd - k, first + i + k);
         }
     }
 
@@ -40,10 +44,7 @@ public:
                     result = string_view(first, last);
             }
         };
-
-        const auto first{str.cbegin()}, last{str.cend()};
-        findPalindromes(first, last, false, func);
-        findPalindromes(first, last, true, func);
+        findPalindromes(str.cbegin(), str.cend(), func);
 
         return string(result);
     }
