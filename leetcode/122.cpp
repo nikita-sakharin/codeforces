1142e685a211697d0624ca64075843ac979829aa
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-08-24 21:01:31 +0100

    leetcode: 122

--- leetcode/122.cpp
+++ leetcode/122.cpp
@@ -1,9 +1,9 @@
 class Solution final {
 public:
     constexpr int maxProfit(const vector<int> &prices) const noexcept {
-        const auto size{prices.size()};
+        const auto length{size(prices)};
         auto result{0};
-        for (auto i{1UZ}; i < size; ++i)
+        for (auto i{1UZ}; i < length; ++i)
             result += max(0, prices[i] - prices[i - 1]);
 
         return result;
