d72ca44ad3b73c0a6191a46630980b1d96710223
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-08-24 11:51:47 +0100

    leetcode: 56

--- leetcode/56.cpp
+++ leetcode/56.cpp
@@ -4,18 +4,18 @@ public:
         vector<vector<int>> &intervals
     ) const noexcept {
         const auto last{intervals.end()};
-        auto left{intervals.begin()};
-        sort(left, last);
+        auto slow{intervals.begin()};
+        sort(slow, last);
 
-        for (auto right{next(left)}; right != last; ++right) {
-            auto &leftValue{*left};
-            const auto &rightValue{*right};
-            if (leftValue[1] >= rightValue[0])
-                leftValue[1] = max(leftValue[1], rightValue[1]);
-            else if (++left != right)
-                iter_swap(left, right);
+        for (auto fast{next(slow)}; fast != last; ++fast) {
+            auto &slowRight{(*slow)[1]};
+            const auto &fastValue{*fast};
+            if (slowRight >= fastValue[0])
+                slowRight = max(slowRight, fastValue[1]);
+            else if (++slow != fast)
+                iter_swap(slow, fast);
         }
-        intervals.resize(size_t(distance(intervals.begin(), left)) + 1);
+        intervals.resize(size_t(distance(intervals.begin(), slow)) + 1);
 
         return move(intervals);
     }
