025c476b4c5b48247e07d699cee8fb3a763232b6
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-09-01 13:10:24 +0100

    leetcode: 11

--- leetcode/11.cpp
+++ leetcode/11.cpp
@@ -3,7 +3,7 @@ public:
     constexpr int maxArea(const vector<int> &height) const noexcept {
         auto result{0};
         auto i{cbegin(height)}, j{--cend(height)};
-        while (i < j) {
+        while (i != j) {
             const auto width{int(j - i)}, minHeight{*i < *j ? *i++ : *j--};
             result = max(result, width * minHeight);
         }
