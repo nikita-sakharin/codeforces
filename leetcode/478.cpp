5a7e7b480651d2583e0e080a404207ae5e3e1aaa
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-09-12 23:51:23 +0100

    leetcode: 478

--- leetcode/478.cpp
+++ leetcode/478.cpp
@@ -6,7 +6,7 @@ private:
         const dbl radius
     ) noexcept {
         const auto i = {0.0, radius}, w = {0.0, 2 / radius};
-        return piecewise_linear_distribution(i.begin(), i.end(), w.begin());
+        return piecewise_linear_distribution(begin(i), end(i), begin(w));
     }
 
     default_random_engine eng{random_device{}()};
