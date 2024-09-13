417710915d181e387840fd7570ee9ff02e8988ba
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-09-13 12:34:03 +0100

    leetcode: 66

--- leetcode/66.cpp
+++ leetcode/66.cpp
@@ -1,16 +1,19 @@
 class Solution final {
+private:
+    static constexpr auto radix{10};
+
 public:
     constexpr vector<int> plusOne(vector<int> &digits) const noexcept {
         auto carry{true};
-        for (ptrdiff_t i(digits.size() - 1); i >= 0 && carry; --i)
-            if (++digits[i] < 10)
+        for (auto i{ssize(digits) - 1}; i >= 0 && carry; --i)
+            if (++digits[i] < radix)
                 carry = false;
             else {
                 carry = true;
                 digits[i] = 0;
             }
         if (carry)
-            digits.insert(digits.cbegin(), 1);
+            digits.insert(cbegin(digits), 1);
         return move(digits);
     }
 };
