961cbf010346e393fe6dc5de1acfd51c6a40df0a
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-09-14 17:51:29 +0100

    leetcode: 112

--- leetcode/112.cpp
+++ leetcode/112.cpp
@@ -14,44 +14,47 @@ private:
     template<class T>
     using Stack = stack<T, vector<T>>;
 
+    static constexpr bool hasRightSibling(
+        const TreeNode * const treeNode,
+        const TreeNode * const parent
+    ) noexcept {
+        return parent->right && treeNode == parent->left;
+    }
+
 public:
-    /*constexpr*/ inline bool hasPathSum(
-        const TreeNode *root,
+    inline bool hasPathSum(
+        const TreeNode *treeNode,
         int targetSum
     ) const noexcept {
-        Stack<const TreeNode *> s{};
-        while (root) {
-            s.push(root);
-            targetSum -= root->val;
-            if (root->left)
-                root = root->left;
+        Stack<const TreeNode *> lifo{};
+        while (treeNode) {
+            lifo.push(treeNode);
+            targetSum -= treeNode->val;
+            if (treeNode->left)
+                treeNode = treeNode->left;
             else
-                root = root->right;
+                treeNode = treeNode->right;
         }
 
-        while (!empty(s)) {
-            if (targetSum == 0)
-                return true;
-
-            const TreeNode *node{};
-            do {
-                node = s.top();
-                targetSum += node->val;
-                s.pop();
-            } while (!empty(s) && (node == s.top()->right || !s.top()->right));
-
-            if (empty(s))
-                break;
-
-            node = s.top()->right;
-            do {
-                s.push(node);
+        while (!empty(lifo)) {
+            const TreeNode *node{lifo.top()->right};
+            while (node) {
+                lifo.push(node);
                 targetSum -= node->val;
                 if (node->left)
                     node = node->left;
                 else
                     node = node->right;
-            } while (node);
+            }
+
+            if (targetSum == 0)
+                return true;
+
+            do {
+                node = lifo.top();
+                targetSum += node->val;
+                lifo.pop();
+            } while (!empty(lifo) && !hasRightSibling(node, lifo.top()));
         }
 
         return false;
