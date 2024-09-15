c0316cb8ee6668ae4b2b7e477abd96a3cb202494
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-09-15 21:25:49 +0100

    leetcode: 26

--- leetcode/26.cpp
+++ leetcode/26.cpp
@@ -1,9 +1,9 @@
 class Solution final {
 public:
     constexpr int removeDuplicates(vector<int> &nums) const noexcept {
-        const auto size{nums.size()};
+        const auto length{nums.size()};
         auto newSize{1UZ};
-        for (auto i{1UZ}; i < size; ++i)
+        for (auto i{1UZ}; i < length; ++i)
             if (nums[i - 1] != nums[i])
                 nums[newSize++] = nums[i];
         nums.resize(newSize);
