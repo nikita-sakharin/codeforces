58526b408c59650c8290f3a00c9a5719a934a250
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-09-15 21:52:27 +0100

    leetcode: 541

--- leetcode/541.cpp
+++ leetcode/541.cpp
@@ -1,13 +1,13 @@
 class Solution final {
 public:
-    inline string reverseStr(string &s, const int k) const noexcept {
-        const auto last{s.end()};
-        auto iter{s.begin()};
+    inline string reverseStr(string &str, const int k) const noexcept {
+        const auto last{end(str)};
+        auto iter{begin(str)};
         while (iter != last) {
             const auto nextIter{iter + min(last - iter, ptrdiff_t(k))};
             reverse(iter, nextIter);
             iter = nextIter + min(last - nextIter, ptrdiff_t(k));
         }
-        return move(s);
+        return move(str);
     }
 };
