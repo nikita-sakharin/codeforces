8d23fc739e0ee654e21c549a68027cf04aebdb1d
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-09-16 00:25:45 +0100

    leetcode: 35

--- leetcode/35.cpp
+++ leetcode/35.cpp
@@ -1,13 +1,30 @@
 class Solution final {
+private:
+    template<class Iter, class T>
+    static constexpr Iter lowerBound(
+        Iter first,
+        Iter last,
+        const T &value
+    ) noexcept {
+        auto length{distance(first, last)};
+        while (length > 0) {
+            const auto half{length >> 1};
+            const auto middle{next(first, half)};
+            if (*middle < value) {
+                first = next(middle);
+                length -= half + 1;
+            } else
+                length = half;
+        }
+        return first;
+    }
+
 public:
     constexpr int searchInsert(
         const vector<int> &nums,
         const int target
     ) const noexcept {
-        const auto size{nums.size()};
-        for (auto i{0UZ}; i < size; ++i)
-            if (nums[i] >= target)
-                return int(i);
-        return size;
+        const auto first{cbegin(nums)};
+        return int(lowerBound(first, cend(nums), target) - first);
     }
 };
