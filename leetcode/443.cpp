ac301d84e8940f01b2681809e5ba3bd333b394c9
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-09-15 21:33:50 +0100

    leetcode: 443

--- leetcode/443.cpp
+++ leetcode/443.cpp
@@ -1,11 +1,11 @@
 class Solution final {
 public:
     constexpr int compress(vector<char> &chars) const noexcept {
-        const auto size{chars.size()};
-        const auto ptrEnd{chars.data() + size};
+        const auto length{size(chars)};
+        const auto ptrEnd{chars.data() + length};
         auto ptr{chars.data() + 1};
         auto cnt{1UZ};
-        for (auto i{1UZ}; i < size; ++i) {
+        for (auto i{1UZ}; i < length; ++i) {
             if (chars[i - 1] != chars[i]) {
                 if (cnt > 1)
                     ptr = to_chars(ptr, ptrEnd, cnt).ptr;
@@ -17,6 +17,6 @@ public:
         if (cnt > 1)
             ptr = to_chars(ptr, ptrEnd, cnt).ptr;
         chars.resize(size_t(ptr - chars.data()));
-        return int(chars.size());
+        return int(size(chars));
     }
 };
