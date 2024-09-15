1e7215194e992bff593ad13b28380d6c68b6f674
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-09-15 16:23:51 +0100

    leetcode: 430

--- leetcode/430.cpp
+++ leetcode/430.cpp
@@ -14,26 +14,28 @@ private:
     using Stack = stack<T, vector<T>>;
 
 public:
-    inline Node *flatten(Node * const head) const noexcept {
+    constexpr Node *flatten(Node * const head) const noexcept {
         if (!head)
             return nullptr;
-        Stack<Node *> s{};
+
+        Stack<Node *> lifo{};
         auto current{head};
-        while (current->next || current->child || !s.empty()) {
+        while (current->next || current->child || !empty(lifo)) {
             if (current->child) {
                 if (current->next)
-                    s.push(current->next);
+                    lifo.push(current->next);
                 current->next = current->child;
                 current->next->prev = current;
                 current->child = nullptr;
             }
             if (!current->next) {
-                current->next = s.top();
+                current->next = lifo.top();
                 current->next->prev = current;
-                s.pop();
+                lifo.pop();
             }
             current = current->next;
         }
+
         return head;
     }
 };
