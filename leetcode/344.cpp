53ec8ec9a4800461211be0c3539fa6feae54297b
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-09-29 17:36:12 +0100

    leetcode: 344

--- leetcode/344.cpp
+++ leetcode/344.cpp
@@ -1,8 +1,35 @@
 class Solution final {
+private:
+    template<class Iter>
+    static constexpr auto category{iterator_traits<Iter>::iterator_category()};
+
+    template<class Iter>
+    static constexpr void reverse(
+        Iter first,
+        Iter last,
+        const bidirectional_iterator_tag
+    ) noexcept {
+        while (first != last && first != --last)
+            iter_swap(first++, last);
+    }
+
+    template<class Iter>
+    static constexpr void reverse(
+        Iter first,
+        Iter last,
+        const random_access_iterator_tag
+    ) noexcept {
+        while (first < last)
+            iter_swap(first++, --last);
+    }
+
+    template<class Iter>
+    static constexpr void reverse(Iter first, Iter last) noexcept {
+        reverse(first, last, category<Iter>);
+    }
+
 public:
     constexpr void reverseString(vector<char> &str) const noexcept {
-        auto first{begin(str)}, last{--end(str)};
-        while (first < last)
-            iter_swap(first++, last--);
+        Solution::reverse(begin(str), end(str));
     }
 };
