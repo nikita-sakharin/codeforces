8f59a7420f389c5fade4ff72cf2fed3798ba395b
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-09-29 18:22:03 +0100

    leetcode: 283

--- leetcode/283.cpp
+++ leetcode/283.cpp
@@ -1,9 +1,9 @@
 class Solution final {
 public:
     constexpr void moveZeroes(vector<int> &nums) const noexcept {
-        const auto size{nums.size()};
+        const auto length{size(nums)};
         auto nonZero{0UZ};
-        for (auto i{0UZ}; i < size; ++i)
+        for (auto i{0UZ}; i < length; ++i)
             if (nums[i] != 0)
                 swap(nums[nonZero++], nums[i]);
     }
