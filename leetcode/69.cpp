9285f5f93ce98de1365756f0426c8a4597083809
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-10-14 17:25:37 +0100

    leetcode: 69

--- leetcode/69.cpp
+++ leetcode/69.cpp
@@ -1,9 +1,12 @@
 class Solution final {
+private:
+    using uint = unsigned;
+
 public:
     constexpr int mySqrt(const int x) const noexcept {
         if (x <= 1)
             return x;
-        auto xCurrent{0}, xNext{x >> 1};
+        auto xCurrent{0}, xNext{1 << ((bit_width(uint(x) - 1) + 1) >> 1)};
         do {
             xCurrent = xNext;
             xNext = (xCurrent + x / xCurrent) >> 1;
