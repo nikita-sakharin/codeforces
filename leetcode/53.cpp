74c88dcc6ec3355d01e24834eadf56e156b9047e
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-11-05 15:33:18 +0000

    leetcode: 53

--- leetcode/53.cpp
+++ leetcode/53.cpp
@@ -3,9 +3,8 @@ public:
     constexpr int maxSubArray(const vector<int> &nums) const noexcept {
         auto sum{0}, result{numeric_limits<int>::min()};
         for (const auto num : nums) {
-            sum += num;
+            sum = max(num, sum + num);
             result = max(result, sum);
-            sum = max(0, sum);
         }
 
         return result;
