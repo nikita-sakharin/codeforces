96f880e9370bc019804f510b0cf4706f56805a70
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-11-05 16:46:30 +0000

    leetcode: 912

--- leetcode/912.cpp
+++ leetcode/912.cpp
@@ -1,27 +1,29 @@
 class Solution final {
 private:
     template<class Iter>
+    using Value = iterator_traits<Iter>::value_type;
+
+    template<class Iter>
     static constexpr auto
         category{typename iterator_traits<Iter>::iterator_category()};
 
-    template<class T, class Container = deque<T>>
+    template<class Iter, class Container = deque<Value<Iter>>>
     class DefaultMerger final {
     private:
-        mutable queue<T, Container> buffer{};
+        mutable queue<Value<Iter>, Container> buffer{};
 
-        template<class Iter>
         constexpr void operator()(
             Iter first,
             const Iter middle,
             const Iter last,
             const forward_iterator_tag
         ) const noexcept {
-            if (middle == last)
+            if (first == middle || middle == last)
                 return;
 
             auto second{middle};
-            auto leftNonEmpty{first != middle};
-            while (leftNonEmpty || !empty(buffer)) {
+            auto leftNonEmpty{true};
+            do {
                 if (leftNonEmpty)
                     buffer.push(move(*first));
                 if (empty(buffer) || (second != last && *second < buffer.front())) {
@@ -33,21 +35,20 @@ private:
                 }
                 ++first;
                 leftNonEmpty = leftNonEmpty && first != middle;
-            }
+            } while (leftNonEmpty || !empty(buffer));
         }
 
-        template<class Iter>
         constexpr void operator()(
             Iter first,
             const Iter middle,
             const Iter last,
             const random_access_iterator_tag
         ) const noexcept {
-            if (middle == last)
+            if (first == middle || middle == last)
                 return;
 
             auto second{middle};
-            while (first < middle || !empty(buffer)) {
+            do {
                 if (first < middle)
                     buffer.push(move(*first));
                 if (empty(buffer) || (second != last && *second < buffer.front())) {
@@ -58,11 +59,10 @@ private:
                     buffer.pop();
                 }
                 ++first;
-            }
+            } while (first < middle || !empty(buffer));
         }
 
     public:
-        template<class Iter>
         constexpr void operator()(
             Iter first,
             const Iter middle,
@@ -72,32 +72,39 @@ private:
         }
     };
 
-    template<class T>
+    template<class Iter, class Container = vector<tuple<Iter, Iter, Iter>>>
     class RotateMerger final {
-    public:
-        template<class Iter>
-        constexpr void operator()(
-            const Iter first,
-            Iter middle,
-            const Iter last
-        ) const noexcept {
-            if (first == middle || middle == last)
-                return;
+    private:
+        mutable stack<tuple<Iter, Iter, Iter>, Container> lifo{};
 
-            const auto leftSize{distance(first, middle)},
-                rightSize{distance(middle, last)};
-            Iter leftIter, rightIter;
-            if (leftSize < rightSize) {
-                rightIter = next(middle, (rightSize + 1) >> 1);
-                leftIter = upper_bound(first, middle, *prev(rightIter));
-                middle = prev(rotate(leftIter, middle, rightIter));
-            } else {
-                leftIter = next(first, (leftSize - 1) >> 1);
-                rightIter = lower_bound(middle, last, *leftIter);
-                middle = rotate(leftIter, middle, rightIter);
-            }
-            (*this)(first, leftIter, middle);
-            (*this)(next(middle), rightIter, last);
+    public:
+        constexpr void operator()(
+            Iter first,
+            Iter middle,
+            Iter last
+        ) const noexcept {
+            lifo.emplace(first, middle, last);
+            do {
+                tie(first, middle, last) = lifo.top();
+                lifo.pop();
+                if (first == middle || middle == last)
+                    continue;
+
+                const auto leftSize{distance(first, middle)},
+                    rightSize{distance(middle, last)};
+                Iter leftIter, rightIter;
+                if (leftSize < rightSize) {
+                    rightIter = next(middle, (rightSize + 1) >> 1);
+                    leftIter = upper_bound(first, middle, *prev(rightIter));
+                    middle = prev(rotate(leftIter, middle, rightIter));
+                } else {
+                    leftIter = next(first, (leftSize - 1) >> 1);
+                    rightIter = lower_bound(middle, last, *leftIter);
+                    middle = rotate(leftIter, middle, rightIter);
+                }
+                lifo.emplace(first, leftIter, middle);
+                lifo.emplace(next(middle), rightIter, last);
+            } while (!empty(lifo));
         }
     };
 
@@ -129,8 +136,8 @@ private:
     }
 
 public:
-    constexpr vector<int> sortArray(vector<int> &nums) const noexcept {
-        mergeSort(begin(nums), end(nums), RotateMerger<int>{});
+    inline vector<int> sortArray(vector<int> &nums) const noexcept {
+        mergeSort(begin(nums), end(nums), RotateMerger<vector<int>::iterator>{});
         return move(nums);
     }
 };
