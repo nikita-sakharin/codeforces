7e7ca5e2ab2e9760518d03d5161313edfd878442
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-11-29 18:49:20 +0000

    leetcode: 912

--- leetcode/912.cpp
+++ leetcode/912.cpp
@@ -135,6 +135,72 @@ private:
         }
     }
 
+    template<class Iter>
+    static constexpr void heapSort(
+        const Iter first,
+        const Iter last
+    ) noexcept {
+        makeHeap(first, last);
+        sortHeap(first, last);
+    }
+
+    template<class Iter>
+    static constexpr void makeHeap(const Iter first, const Iter last) noexcept {
+        if (first == last)
+            return;
+
+        for (auto iter{first}; iter != last; ++iter)
+            pushHeap(first, next(iter));
+    }
+
+    template<class Iter>
+    static constexpr void sortHeap(const Iter first, Iter last) noexcept {
+        for (; first != last; --last)
+            popHeap(first, last);
+    }
+
+    template<class Iter>
+    static constexpr void popHeap(const Iter first, Iter last) noexcept {
+        iter_swap(first, --last);
+        siftDown(first, first, last);
+    }
+
+    template<class Iter>
+    static constexpr void pushHeap(const Iter first, const Iter last) noexcept {
+        siftUp(first, prev(last));
+    }
+
+    template<class Iter>
+    static constexpr void siftDown(
+        const Iter first,
+        Iter element,
+        const Iter last
+    ) noexcept {
+        const auto half{next(first, distance(first, last) >> 1)};
+        while (element < half) {
+            auto child{next(element, distance(first, element) + 1)};
+            if (const auto sibling{next(child)};
+                sibling != last && *child < *sibling
+            )
+                ++child;
+            if (*element >= *child)
+                break;
+            iter_swap(element, child);
+            element = child;
+        }
+    }
+
+    template<class Iter>
+    static constexpr void siftUp(const Iter first, Iter element) noexcept {
+        while (element != first) {
+            const auto parent{next(first, (distance(first, element) - 1) >> 1)};
+            if (*parent >= *element)
+                break;
+            iter_swap(parent, element);
+            element = parent;
+        }
+    }
+
 public:
     inline vector<int> sortArray(vector<int> &nums) const noexcept {
         mergeSort(begin(nums), end(nums), RotateMerger<vector<int>::iterator>{});
