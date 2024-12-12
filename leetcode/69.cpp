8d5622e3a3c767d15db5cc022f0458332d42af75
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-12-12 16:27:33 +0000

    leetcode: 69

--- leetcode/69.cpp
+++ leetcode/69.cpp
@@ -6,11 +6,13 @@ public:
     constexpr int mySqrt(const int x) const noexcept {
         if (x <= 1)
             return x;
+
         auto xCurrent{0}, xNext{1 << ((bit_width(uint(x) - 1) + 1) >> 1)};
         do {
             xCurrent = xNext;
             xNext = (xCurrent + x / xCurrent) >> 1;
         } while (xNext < xCurrent);
+
         return xCurrent;
     }
 };
