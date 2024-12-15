2c929f4004b634b6b24e03c451c10755061dd7dd
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-12-15 16:00:04 +0000

    leetcode: 69

--- leetcode/69.cpp
+++ leetcode/69.cpp
@@ -2,17 +2,22 @@ class Solution final {
 private:
     using uint = unsigned;
 
+    template<class T>
+    static constexpr T isqrt(const T n) noexcept {
+        if (n <= T{1})
+            return n;
+
+        T iCurrent{0}, iNext{T(T{1} << ((bit_width(T(n - 1)) + 1) >> 1))};
+        do {
+            iCurrent = iNext;
+            iNext = T((iCurrent + n / iCurrent) >> 1);
+        } while (iNext < iCurrent);
+
+        return iCurrent;
+    }
+
 public:
     constexpr int mySqrt(const int x) const noexcept {
-        if (x <= 1)
-            return x;
-
-        auto xCurrent{0}, xNext{1 << ((bit_width(uint(x) - 1) + 1) >> 1)};
-        do {
-            xCurrent = xNext;
-            xNext = (xCurrent + x / xCurrent) >> 1;
-        } while (xNext < xCurrent);
-
-        return xCurrent;
+        return isqrt(uint(x));
     }
 };
