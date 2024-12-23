c4c481ba7035dea6bbcc8e204dbe493872b09353
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-12-23 09:37:55 +0000

    leetcode: 637

--- leetcode/637.cpp
+++ leetcode/637.cpp
@@ -19,28 +19,21 @@ public:
         vector<dbl> result{};
         queue<const TreeNode *> fifo{};
         fifo.push(root);
-        fifo.emplace();
 
-        auto sum{0LL};
-        auto count{0UZ};
         do {
-            const auto treeNode{fifo.front()};
-            fifo.pop();
-            if (treeNode) {
+            auto sum{0LL};
+            auto count{0UZ};
+            for (auto length{size(fifo)}; length != 0; --length) {
+                const auto treeNode{fifo.front()};
+                fifo.pop();
                 sum += treeNode->val;
                 ++count;
                 if (treeNode->left)
                     fifo.push(treeNode->left);
                 if (treeNode->right)
                     fifo.push(treeNode->right);
-            } else {
-                result.push_back(dbl(sum) / count);
-                if (!empty(fifo)) {
-                    fifo.emplace();
-                    sum = 0;
-                    count = 0;
-                }
             }
+            result.push_back(dbl(sum) / count);
         } while (!empty(fifo));
 
         return result;
