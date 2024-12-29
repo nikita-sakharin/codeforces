ab593de09691280a96ce5bb45ecae79c12d75a4c
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-12-29 17:56:56 +0000

    leetcode: 103

---
+++ leetcode/103.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution final {
+public:
+    constexpr vector<vector<int>> zigzagLevelOrder(
+        const TreeNode * const root
+    ) const noexcept {
+        vector<vector<int>> result{};
+        if (!root)
+            return result;
+
+        queue<const TreeNode *> fifo{};
+        fifo.push(root);
+
+        do {
+            const auto length{size(fifo)}, mask{-(result.size() & 1)};
+            auto &level{result.emplace_back()};
+            level.resize(length);
+            for (auto i{0UZ}; i < length; ++i) {
+                const auto treeNode{fifo.front()};
+                fifo.pop();
+                level[(length & mask) + (mask ^ i)] = treeNode->val;
+                if (treeNode->left)
+                    fifo.push(treeNode->left);
+                if (treeNode->right)
+                    fifo.push(treeNode->right);
+            }
+        } while (!empty(fifo));
+
+        return result;
+    }
+};
